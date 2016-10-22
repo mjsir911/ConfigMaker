0a2d951a930bfa3714a5993fb9753a43a7b881fa
Marco Sirabella <msirabel@gmail.com>
2016-10-22 11:25:03 -0400

    - Added more python2.7 compatibility

--- stripped.py
+++ stripped.py
@@ -24,7 +24,7 @@ __module__      = ""
 
 class MyWindow(PySide.QtGui.QMainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super(MyWindow, self).__init__()
 
 
 
