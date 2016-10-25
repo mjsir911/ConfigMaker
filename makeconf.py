3badbeec2c5ebf9a3b871ff185f28e52de904b40
Marco Sirabella <msirabel@gmail.com>
2016-10-25 18:59:45 -0400

    - Fixed nonetype error

--- makeconf.py
+++ makeconf.py
@@ -125,17 +125,17 @@ class MyWindow(PySide.QtGui.QMainWindow):
         for y, x in enumerate(dictionary['ratings']):
             self.widget.ratingsWidget.add()
             self.widget.ratingsWidget.rName.setItemText(y, x['name'])
-            currentratingswidget = self.widget.ratingsWidget.alltheratings[y]
-            currentratingswidget.question.setText(x['question'])
-            currentratingswidget.rType.setCurrentIndex(
+            currentwidget = self.widget.ratingsWidget.alltheratings[y]
+            currentwidget.question.setText(x['question'])
+            currentwidget.rType.setCurrentIndex(
                     responses[x['subtype']])
             if x['subtype'] != "free":
-                currentratingswidget.options.questionNum.setValue(
+                currentwidget.options.questionNum.setValue(
                         len(x['options']))
-            for c, d in enumerate(currentratingswidget.options.responses):
-                print(d)
-                print(x['options'][c])
-                d.selection.setText(x['options'][c])
+            #print(len(currentwidget.options.responses))
+            if x['options']:
+                for c, d in enumerate(x['options']):
+                    currentwidget.options.responses[c].selection.setText(d)
 
 
 
