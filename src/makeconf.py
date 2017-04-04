0720438aa79093ae574eacb71c4243e580bc13df
Marco Sirabella <msirabel@gmail.com>
2017-04-04 17:11:17 -0400

    - Added broken line

--- src/makeconf.py
+++ src/makeconf.py
@@ -223,7 +223,7 @@ class MyWindow(PySide.QtGui.QMainWindow):
             json_trial = data['trials'][-1]
             #WIP
             json_trial['description'] = "description work in progress"
-            json_trial['program'] = 1
+            json_trial['program'] = trials
 
             #END OF WIP
             json_trial['targets'] = targets = []
