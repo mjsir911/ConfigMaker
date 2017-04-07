0eb8c2d6e0fe24e228c4a8ed28cea1c65c2018eb
Kevin Cole <dc.loco@gmail.com>
2017-04-07 15:31:16 -0400

    - Separated one-time-only installation script from compilation script

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 # Written by Kevin Cole <kevin.cole@novawebcoop.org> 2017.04.02
 #
 # This determines if we're in a Python virtual environment, and,
-# if so, fetches and installs all the prerequisites.
+# if so, creates a Mac OS X ".app"
 #
 # Note: This installer was designed with and for Mac OS X 10.12.4
 # (Sierra).  py2app in particular, is used to "bundle" the python code
@@ -16,7 +16,15 @@ then
   echo "You must run this from a Python virtual environment."
   echo "(Read up on virtualenv for more info.)"
 else
-  pip install -U pip
-  pip install -r requirements.txt
-  python2.7 $VIRTUAL_ENV/bin/pyside_postinstall.py -install
+  git checkout stable
+  git fetch upstream
+  git merge upstream/stable
+  git push
+  git pull
+  rm -rf build dist
+  rm setup.py
+  py2applet --make-setup src/makeconf.py
+  perl -p -i -e "s|True|False|g;" setup.py
+  python2.7 setup.py py2app -A
+  rm setup.py
 fi
