70c303b1a042b7ecf44b2a19e94e44693e8846aa
Kevin Cole <dc.loco@gmail.com>
2017-09-18 13:07:57 -0400

    - Change unedited loudspeaker button color from black to red

--- src/presets.py
+++ src/presets.py
@@ -119,7 +119,7 @@ class FancyCircle(LocalizationPane.ControlPane):
         for i in range(8):
             for button in ('louder', 'softer'):
                 self.speakers[i][button].hide()
-            self.speakers[i]['select'].setStyleSheet(self.style.format('black'))
+            self.speakers[i]['select'].setStyleSheet(self.style.format('red'))
         shared.logger.debug("finishing fancy circle")
 
     def paintEvent(self, event):
